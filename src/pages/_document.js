76b4ba71b75ad06ccf9c3cc1ba65662318345bd7
Marvin Bernd <hello@marvinbernd.de>
2024-01-09 11:32:23 +0200

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { createStyleRegistry, StyleRegistry } from "@kuma-ui/core";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const registry = createStyleRegistry();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            (
+              <StyleRegistry registry={registry}>
+                <App {...props} />
+              </StyleRegistry>
+            ),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, registry.styles()],
+      };
+    } finally {
+      registry.flush();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
